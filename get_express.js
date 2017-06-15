da49ebb1945fb471841100203ce984532a2d6053
quangakgl <quangprolathe@gmail.com>
2017-06-15 11:24:40 +0700

    update get_express.js

--- get_express.js
+++ get_express.js
@@ -14,30 +14,19 @@ nunjucks.configure('views', {
     watch: true
 });
 
-//app.use(express.static('views'));
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.engine('html', nunjucks.render);
 app.set('view engine', 'html');
 
-// app.get('/', (req, res) => {
-//     res.render('index');
-// });
-// app.post('/', (req, res) => {
-//     let a = parseFloat(req.body.a) ;
-//
-//     let b = parseFloat(req.body.b) ;
-//     let c = a + b ;
-//
-//     res.render('index', {c: c});
-// })
 app.get('/', (req, res) => {
     let a = parseFloat(req.query.a) ;
     let b = parseFloat(req.query.b) ;
     let c = a + b ;
 
-    res.render('index.html', {c: c});
+    res.render('index.html', {a: a, b: b,c: c});
 })
 app.listen(4000, () => {
     console.log('listen port 4000')
--- post_express.js
+++ post_express.js
@@ -1,43 +1,38 @@
 /**
  * Created by quang on 14/06/2017.
  */
+//các mô đum cần dùng
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
 const bodyParser = require("body-parser");
-
+// cấu hình của nunjucks
 nunjucks.configure('views', {
     autoescape: true,
     cache: false,
     express: app,
     watch: true
 });
-
-//app.use(express.static('views'));
+    // sử dụng midleware body-parser
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// sử dụng engine html dùng nunjucks để render ra trang html
 app.engine('html', nunjucks.render);
-app.set('view engine', 'html');
 
+app.set('view engine', 'html');
+// render ra trang html
 app.get('/', (req, res) => {
     res.render('index2');
 });
+//
 app.post('/', (req, res) => {
     let a = parseFloat(req.body.a) ;
-
     let b = parseFloat(req.body.b) ;
     let c = a + b ;
-
-    res.render('index2', {c: c});
+    res.render('index2', {a: a, b: b,c: c});
 })
-// app.get('/', (req, res) => {
-//     let a = parseFloat(req.query.a) ;
-//     let b = parseFloat(req.query.b) ;
-//     let c = a + b ;
-//
-//     res.render('index', {c: c});
-// })
+
 app.listen(4000, () => {
     console.log('listen port 4000')
 })
